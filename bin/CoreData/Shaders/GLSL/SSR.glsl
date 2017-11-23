23c5dc3e51cff06259c8ed43c15343499f87114f
MonkeyFirst <monkey3dcg@gmail.com>
2017-11-23 15:42:46 +0700

    add edgeFactor and reflection multipler

--- bin/CoreData/Shaders/GLSL/SSR.glsl
+++ bin/CoreData/Shaders/GLSL/SSR.glsl
@@ -2,6 +2,7 @@
 #include "Samplers.glsl"
 #include "Transform.glsl"
 #include "ScreenPos.glsl"
+#line 5
 
 varying vec2 vTexCoord;
 varying vec2 vScreenPos;
@@ -9,12 +10,14 @@ varying vec3 vFarRay;
 varying vec3 vViewFarRay;
 
 #ifdef COMPILEPS
-uniform vec2 cSSRHInvSize;
+const float cSSRStep = 0.5;                    // default = 0.5, for large view distances need increase step                   
+const int cSSRMaxSteps = 32;                  // default = 32
+const int cSSRNumBinarySearchSteps = 16;      // default = 16  
+const float cSSRReflectionFalloff = 0.1;       // 0...1.0
+const float cSSRClipSSRDistance = 200;         // distance from camera to clip plane
 #endif
-#line 14
 
 #ifdef COMPILEVS
-
 vec3 GetViewFarRaySS(vec2 ss) 
 {
   vec4 cameraRay = vec4(ss.xy * vec2(2.0, 2.0) - vec2(1.0, 1.0), 1.0, 1.0);
@@ -38,32 +41,12 @@ void VS()
 
 #ifdef COMPILEPS
 
-vec3 GetViewFarRaySSPS(vec2 ss) 
-{
-  vec4 cameraRay = vec4(ss.xy * vec2(2.0,2.0) - vec2(1.0, 1.0), 1.0, 1.0);
-  cameraRay = cameraRay * cProjInvPS;
-  cameraRay.xyz = cameraRay.xyz / cameraRay.w;
-  return cameraRay.xyz;
-}
-
 vec2 GetScreenPosPreDivPS(vec4 clipPos)
 {
     return vec2(
         clipPos.x / clipPos.w * cGBufferOffsetsPS.z + cGBufferOffsetsPS.x,
         clipPos.y / clipPos.w * cGBufferOffsetsPS.w + cGBufferOffsetsPS.y);
 }
-
-vec3 GetViewPosition(vec2 ssPos, float depth)
-{
-    // eye_z = depth(0=camera .. 1=far) * far
-    float eye_z = depth * cFrustumSizePS.z;
-    return vec3(ssPos * cProjInfo.xy + cProjInfo.zw, 1.0) * eye_z;
-}
-
-const float step = 0.5;
-const float minRayStep = 0.1;
-const float maxSteps = 32;
-const int numBinarySearchSteps = 16;
 
 vec3 fresnelSchlick(float cosTheta, vec3 F0)
 {
@@ -83,24 +66,25 @@ vec3 hash(vec3 a)
 
 vec3 BinarySearch(inout vec3 dir, inout vec3 hitCoord, inout float dDepth)
 {
-    float depth;
+    float sampledDepth;
     vec4 projectedCoord;
 
-    for(int i = 0; i < numBinarySearchSteps; i++)
+    for(int i = 0; i < cSSRNumBinarySearchSteps; i++)
     {
         projectedCoord = vec4(hitCoord, 1.0) * cProjPS;
-        //projectedCoord.xy /= projectedCoord.w;
-        //projectedCoord.xy = projectedCoord.xy * 0.5 + 0.5;
-        projectedCoord.xy = GetScreenPosPreDivPS(projectedCoord);
+        projectedCoord.xy /= projectedCoord.w;
+        projectedCoord.xy = projectedCoord.xy * 0.5 + 0.5;
+        
+        //projectedCoord.xy = GetScreenPosPreDivPS(projectedCoord);
  
         #ifdef HWDEPTH
-            float depth = ReconstructDepth(texture2D(sDepthBuffer, projectedCoord.xy).r);
+            sampledDepth = ReconstructDepth(texture2D(sDepthBuffer, projectedCoord.xy).r);
         #else
-            float depth = DecodeDepth(texture2D(sDepthBuffer, projectedCoord.xy).rgb);
+            sampledDepth = DecodeDepth(texture2D(sDepthBuffer, projectedCoord.xy).rgb);
         #endif
         
         //vec3 viewPos = GetViewPosition(projectedCoord.xy, depth);
-        vec3 viewPos = vViewFarRay * -depth;
+        vec3 viewPos = vViewFarRay * -sampledDepth;
         dDepth = hitCoord.z - viewPos.z;
 
         dir *= 0.5;
@@ -112,52 +96,52 @@ vec3 BinarySearch(inout vec3 dir, inout vec3 hitCoord, inout float dDepth)
     }
 
     projectedCoord = vec4(hitCoord, 1.0) * cProjPS;
-    //projectedCoord.xy /= projectedCoord.w;
-    //projectedCoord.xy = projectedCoord.xy * 0.5 + 0.5;
-    projectedCoord.xy = GetScreenPosPreDivPS(projectedCoord);
- 
-    return vec3(projectedCoord.xy, -depth);
+    projectedCoord.xy /= projectedCoord.w;
+    projectedCoord.xy = projectedCoord.xy * 0.5 + 0.5;
+    
+    //projectedCoord.xy = GetScreenPosPreDivPS(projectedCoord);
+    return vec3(projectedCoord.xy, -sampledDepth);
 }
 
 vec4 RayMarch(vec3 dir, inout vec3 hitCoord, inout float dDepth)
 {
-    dir *= step;
-    float depth;
-    vec4 projectedCoord;
+    dir *= cSSRStep;
+    float depth = 0;
+    vec4 projectedCoord = vec4(0,0,0,0);
 
-    for(int i = 0; i < maxSteps; i++)
+    for(int i = 0; i < cSSRMaxSteps; i++)
     {
         hitCoord += dir;
         
         // View-space hitCoord to clip-space
         projectedCoord =  vec4(hitCoord, 1.0) * cProjPS;
         // Project on screen
-        //projectedCoord.xy /= projectedCoord.w;
-        //projectedCoord.xy = projectedCoord.xy * 0.5 + 0.5;        
-        projectedCoord.xy = GetScreenPosPreDivPS(projectedCoord);
-        
+        projectedCoord.xy /= projectedCoord.w;
+        projectedCoord.xy = projectedCoord.xy * 0.5 + 0.5;        
+        //projectedCoord.xy = GetScreenPosPreDivPS(projectedCoord);
         
         #ifdef HWDEPTH
-            float SampledDepth = ReconstructDepth(texture2D(sDepthBuffer, projectedCoord.xy).r);
+            float sampledDepth = ReconstructDepth(texture2D(sDepthBuffer, projectedCoord.xy).r);
         #else
-            float SampledDepth = DecodeDepth(texture2D(sDepthBuffer, projectedCoord.xy).rgb);
+            float sampledDepth = DecodeDepth(texture2D(sDepthBuffer, projectedCoord.xy).rgb);
         #endif        
         
         // Convert ScreenSpace position to ViewSpace position
         //vec3 viewPos = GetViewPosition(projectedCoord.xy, depth);
-        vec3 viewPos = vViewFarRay * -SampledDepth;
+        vec3 viewPos = vViewFarRay * -sampledDepth;
         depth = viewPos.z;
         
         // ≈сли позици€ слишком далеко от камеры
-        if(depth > 0)
+        if(depth < -cSSRClipSSRDistance)
             continue;
         
         
-        float fDepthDiff = hitCoord.z - depth;
+        //float fDepthDiff = hitCoord.z - depth;
+        float fDepthDiff = depth - hitCoord.z;
             
-        //if((dir.z - dDepth) < 1.0)
+        //if((dir.z - fDepthDiff ) >= -10.0)
         {
-            if(fDepthDiff <= 0.0)
+            if(fDepthDiff >= 0.0)
             {   
                 vec4 Result;
                 Result = vec4(BinarySearch(dir, hitCoord, fDepthDiff), 1.0);
@@ -168,7 +152,6 @@ vec4 RayMarch(vec3 dir, inout vec3 hitCoord, inout float dDepth)
             }
         }
     }
- 
     
     return vec4(projectedCoord.xy, depth, 0.0);
 }
@@ -189,16 +172,8 @@ void PS()
     float specPower = normalInput.a;
     float ssr = specPower;
     
-    //if (ssr < 0.01) // 0..1
-    //  discard;
-  
-    //vec3 viewPos = GetViewPosition(vScreenPos, depth);
-    
     // Restore view position
     vec3 viewPos = vViewFarRay * -depth;
-    
-    // Restore world position
-    vec3 worldPos = vFarRay * -depth; // wp used for generate jitt function
     
     // Transform world normal into view normal
     vec3 viewNormal = normalize(cViewTransposedPS * normal);
@@ -208,28 +183,52 @@ void PS()
     vec3 hitPos = viewPos;
     vec3 reflected = normalize(reflect(normalize(viewPos), viewNormal));
     
-    // Jitt
     float dDepth;
-    vec3 jitt = mix(vec3(0.0), vec3(hash(worldPos)), ssr / 40.0);
     
-    //vec4 coords = RayMarch(reflected * max(minRayStep, -viewPos.z), hitPos, dDepth);
-    
-    //vec4 coords = RayMarch(reflected, hitPos, dDepth); // clean mirror
+    #if 1
+    // Use Jitt
+    // Restore world position
+    vec3 worldPos = vFarRay * -depth; // wp used for generate jitt function
+    vec3 jitt = mix(vec3(0.0), vec3(hash(worldPos)), ssr / 50.0);
     vec4 coords = RayMarch( jitt + reflected, hitPos, dDepth); // dirty mirror with jitt
+    #else
     
+    vec4 coords = RayMarch(reflected, hitPos, dDepth); // clean mirror
+    #endif
+       
     // Use fresnel function to kill "lost information" on front-faced surfaces to camera  
-    float fresnel = clamp(pow(1 - dot(normalize(viewPos), viewNormal), 1), 0, 1);
+    #if 0
+      // fresnelSchlick version
+      vec3 F0 = vec3(0.04); 
+      F0 = mix(F0, albedo, ssr);
+      vec3 fresnel = fresnelSchlick(max(dot(normalize(viewNormal), normalize(viewPos)), 0.0), F0);
+    #else
+      // simple fresnel version
+      vec3 fresnel = vec3(1.0) * clamp(pow(1 - dot(normalize(viewPos), viewNormal), 1), 0, 1);
+    #endif
     
     // read reflected color from current frame
     vec3 reflectionColor = texture2D(sDiffMap, coords.xy, 0).rgb;
-    // tint for ssr surfaced (used only while doing shader's debug)
-    vec3 ssrTint = vec3(0.5, 0.5, 1.0);
+    
+    vec2 dCoords = smoothstep(0.3, 0.6, abs(vec2(0.5, 0.5) - coords.xy));
+    
+    float screenEdgeFactor = clamp(1.0 - (dCoords.x + dCoords.y), 0.0, 1.0);
+    
+    float reflectionMultiplier = pow(ssr, cSSRReflectionFalloff) * screenEdgeFactor * -reflected.z;
     
     #if 1
     
-    gl_FragColor = vec4(albedo + (reflectionColor * ssr * fresnel * ssrTint), specIntensity);
+    // tint for ssr surfaced (used only while doing shader's debug)
+    //vec3 ssrTint = vec3(0.5, 0.5, 1.0);
+    //gl_FragColor = vec4(albedo + (reflectionColor * ssr * fresnel * ssrTint), specIntensity);
     
-    #else // DEBUG 
+    gl_FragColor = vec4(albedo + (reflectionColor * clamp(reflectionMultiplier, 0.0, 1.0) * fresnel), specIntensity);
+    
+    #else // DEBUG
+    //gl_FragColor = vec4(reflectionMultiplier * fresnel,1);
+    //gl_FragColor = vec4(screenEdgefactor,screenEdgefactor,screenEdgefactor, 1); 
+    //gl_FragColor = vec4(dCoords,1, 1);
+    //gl_FragColor = vec4(fresnel, 1);
     //gl_FragColor = vec4(albedo, 1);
     //gl_FragColor = vec4(viewPos, 1.0);
     //gl_FragColor = vec4(viewNormal, 1.0);
